c6077493af1b24ace2b54ed2be6aae0f5fb4fc0e
Syuney-mls <s.takahashi.mls@gmail.com>
2025-11-22 16:34:28 +0900

    fix main.jsx error

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css' // ← この行が超重要！！
+import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
@@ -8,20 +8,3 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
-```
-
----
-
-### 手順3：修正をGitHubに送る
-ファイルを作ったり直したりしたら、また以下の3点セットで送信です！
-
-```bash
-git add .
-```
-
-```bash
-git commit -m "fix css config"
-```
-
-```bash
-git push
